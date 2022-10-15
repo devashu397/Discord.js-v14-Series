1d67450f2dd70c51ad5f99b7d1055c694231f30d
Ashutosh Swamy <76511506+ItzAshu397@users.noreply.github.com>
2022-10-15 20:58:08 +0530

    Discord.js v14 - Part 1 & 2

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+const config = require("./config.json");
 const discord = require("discord.js");
+const fs = require("node:fs");
+const path = require("node:path");
 
 const client = new discord.Client({
   intents: [
@@ -11,4 +14,35 @@ client.once("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
-client.login("your bot token");
+client.commands = new discord.Collection();
+
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  client.commands.set(command.data.name, command);
+}
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+
+  if (!command) return;
+
+  try {
+    await command.execute(client, interaction);
+  } catch (err) {
+    console.log(err);
+    await interaction.reply({
+      content: "There was an error while executing this command...",
+      ephemeral: true,
+    });
+  }
+});
+
+client.login(config.bot_token);
